02472a520492f10c24f8faaffdb81a77e79a0239
Alex Malos <malosalexander@gmail.com>
2022-06-06 21:58:54 -0400

    fixed indentation in todo board

--- todo_board/todo_board.rb
+++ todo_board/todo_board.rb
@@ -10,36 +10,36 @@ class TodoBoard
         cmd, label, *args  = gets.chomp.split
         
         case cmd
-        when 'mklist'
-            @board[label] = List.new(label)
-        when 'ls'
-            @board.each_key { |key| puts " #{key}" }
-        when 'showall'
-            @board.each_value(&:print)
-        when 'mktodo'
-            @board[label].add_item(*args)
-        when 'toggle'
-            @board[label].toggle_item(*args.map(&:to_i))
-        when 'rm'
-            @board[label].remove_item(*args.map(&:to_i))
-        when 'purge'
-            @board[label].purge
-        when 'up'
-            @board[label].up(*args.map(&:to_i))
-        when 'down'
-            @board[label].down(*args.map(&:to_i))
-        when 'swap'
-            @board[label].swap(*args.map(&:to_i))
-        when 'sort'
-            @board[label].sort_by_date!
-        when 'priority'
-            @board[label].print_priority
-        when 'print'
-            args.empty? ? @board[label].print : @board[label].print_full_item(*args.map(&:to_i))
-        when 'quit'
-            return false
-        else
-            print "Sorry, that command is not recognized."
+            when 'mklist'
+                @board[label] = List.new(label)
+            when 'ls'
+                @board.each_key { |key| puts " #{key}" }
+            when 'showall'
+                @board.each_value(&:print)
+            when 'mktodo'
+                @board[label].add_item(*args)
+            when 'toggle'
+                @board[label].toggle_item(*args.map(&:to_i))
+            when 'rm'
+                @board[label].remove_item(*args.map(&:to_i))
+            when 'purge'
+                @board[label].purge
+            when 'up'
+                @board[label].up(*args.map(&:to_i))
+            when 'down'
+                @board[label].down(*args.map(&:to_i))
+            when 'swap'
+                @board[label].swap(*args.map(&:to_i))
+            when 'sort'
+                @board[label].sort_by_date!
+            when 'priority'
+                @board[label].print_priority
+            when 'print'
+                args.empty? ? @board[label].print : @board[label].print_full_item(*args.map(&:to_i))
+            when 'quit'
+                return false
+            else
+                print "Sorry, that command is not recognized."
         end
 
         true
